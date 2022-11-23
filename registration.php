60ba810a93b77dd21ec16e8407e9fb9a6def58b3
kleysla eshily de souza couto <kleysla.souza@academico.ifrn.edu.br>
2022-11-23 14:15:35 -0300

    teste

---
+++ registration.php
@@ -0,0 +1,31 @@
+<?php
+require 'config.php';
+if(!empty($_SESSION["id"])){
+  header("Location: index.php");
+}
+if(isset($_POST["submit"])){
+  $name = $_POST["name"];
+  $username = $_POST["username"];
+  $email = $_POST["email"];
+  $password = $_POST["password"];
+  $confirmpassword = $_POST["confirmpassword"];
+  $duplicate = mysqli_query($conn, "SELECT * FROM tb_user WHERE username = '$username' OR email = '$email'");
+  if(mysqli_num_rows($duplicate) > 0){
+    echo
+    "<script> alert('O nome de usuário ou e-mail já foi usado'); </script>";
+  }
+  else{
+    if($password == $confirmpassword){
+      $query = "INSERT INTO tb_user VALUES('','$name','$username','$email','$password')";
+      mysqli_query($conn, $query);
+      echo
+      "<script> alert('Registrado com sucesso'); </script>";
+    }
+    else{
+      echo
+      "<script> alert('A senha não corresponde'); </script>";
+    }
+  }
+}
+?>
+
